b6c7aaeb46fe85656c2114f3cc6c233f8cb5d723
caro-turnbull <caro.turnbull@gmail.com>
2023-08-23 09:15:59 -0400

    parameterized input for sql injection prevention

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,16 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id as id, students.name as student, cohorts.name as cohort
-FROM students
-JOIN cohorts on cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const queryString = `
+  SELECT students.id as id, students.name as student, cohorts.name as cohort
+  FROM students
+  JOIN cohorts on cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit]
+
+pool.query(queryString, values)
+
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort.`)
   })
-// .catch(err => console.error('query error', err.stack));
+ .catch(err => console.error('query error', err));
 })
